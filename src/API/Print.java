7d19784b505a8f8bdefed4085dbc236179b98950
XUMU <xmgeorge06@gmail.com>
2026-04-10 16:55:29 +0300

    make working printing that prints the whole frame correctly

--- src/API/Print.java
+++ src/API/Print.java
@@ -9,39 +9,31 @@ public class Print {
     public void PrintFrame(JPanel notesPanel) {
 
         PrinterJob job = PrinterJob.getPrinterJob();
-
         PageFormat pf = job.defaultPage();
-        pf.setOrientation(PageFormat.PORTRAIT); // change if you want LANDSCAPE
+        pf.setOrientation(PageFormat.PORTRAIT);
 
-        // 🔥 VERY IMPORTANT: ensure correct size before printing
         notesPanel.setSize(notesPanel.getPreferredSize());
+        notesPanel.validate();
 
         job.setPrintable(new Printable() {
 
             @Override
             public int print(Graphics g, PageFormat pageFormat, int pageIndex) throws PrinterException {
 
-                Graphics2D g2d = (Graphics2D) g;
-
-                double pageHeight = pageFormat.getImageableHeight();
-                double panelHeight = notesPanel.getHeight();
-
-                // calculate total pages
-                int totalPages = (int) Math.ceil(panelHeight / pageHeight);
-
-                if (pageIndex >= totalPages) {
+                if (pageIndex > 0)
+                {
                     return NO_SUCH_PAGE;
                 }
 
-                // move to printable area
+                Graphics2D g2d = (Graphics2D) g;
                 g2d.translate(pageFormat.getImageableX(), pageFormat.getImageableY());
 
-                // move view for current page
-                g2d.translate(0, -pageIndex * pageHeight);
+                double scaleX = pageFormat.getImageableWidth() / notesPanel.getWidth();
+                double scaleY = pageFormat.getImageableHeight() / notesPanel.getHeight();
 
-                // print panel
+                g2d.scale(scaleX, scaleY);
+
                 notesPanel.printAll(g2d);
-
                 return PAGE_EXISTS;
             }
         }, pf);
